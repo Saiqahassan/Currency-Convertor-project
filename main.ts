8bf78db706a8fc94e57b20a5a9b864d95319499e
Saiqahassan <saiqahassan32@gmail.com>
2024-05-25 14:13:53 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.blue.bold("\n \t Welcome to Saiqa - Currency Convertor"));
+
+// Define the list of currencies and their exchange rates
+ let exchange_rate: any = {
+    "USD": 1,
+    "EUR": 0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 277.70,
+}
+// Select the currencies to convert from and to
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: chalk.yellow("Select the currency to convert from:"),
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"],
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: chalk.red("Select the currency to convert into:"),
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"],
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: chalk.blueBright("Enter the amount to convert:"),
+    }
+]);
+//  Perform currency conversion by using formula
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount;
+
+// formula of conversion
+let base_amount = amount / from_amount;
+let converted_amount = base_amount * to_amount;
+
+console.log(`Converted amount = ${chalk.greenBright(converted_amount.toFixed(2))}`);
